758e41b57d6e45d791e2a83dcfd93ce5453454f6
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-09 22:18:21 +0630

    update: minor update

--- Tuto_06.WebApi/Controllers/CustomersController.cs
+++ Tuto_06.WebApi/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using Database2.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Tuto_06.WebApi.Repos;
 
 namespace Tuto_06.WebApi.Controllers
 {
@@ -24,6 +25,7 @@ namespace Tuto_06.WebApi.Controllers
             return Ok(new { customers,messgae="success"});
         }
 
+   
         //[HttpGet("/api/[controller]/{id}")]
         [HttpGet]
         [Route("/api/[controller]/{id}")]
@@ -59,6 +61,48 @@ namespace Tuto_06.WebApi.Controllers
             return Ok(new{ name , id ,age ,token });
         }
 
+        [HttpPut("{customerName}")]
+        //[Route("/api/[controller]/update/{customerName}")]
+        public IActionResult Update([FromRoute] string customerName, [FromBody] Customer customer)
+        {
+            var oldCustomer = this._db.Customers.AsNoTracking().Where(c => c.CustomerId.ToString() == customerName.ToString()).FirstOrDefault();
+            if (oldCustomer is null)
+            {
+                return BadRequest("Customer not Found!");
+            }
 
+            oldCustomer.CompanyName = customer.CompanyName;
+
+            this._db.Entry(oldCustomer).State = EntityState.Modified;
+            int res = this._db.SaveChanges();
+            if (res <= 0)
+            {
+                return BadRequest("Customer update fail");
+            }
+            return Ok(customerName);
+        }
+
+        [HttpDelete("{customerName}")]
+        public IActionResult Delete([FromRoute] string customerName)
+        {
+            var customer = this._db.Customers.AsNoTracking().Where(c => c.CustomerId.ToString() == customerName).FirstOrDefault();
+
+            if(customer is null)
+            {
+                return BadRequest("customer not found!");
+            }
+
+            this._db.Entry(customer).State = EntityState.Deleted;
+            int res = this._db.SaveChanges();
+            return res >= 1 ? Ok(customerName) : BadRequest();
+        }
+
+        [HttpGet]
+        [Route("/api/[controller]/check/{name}")]
+        public IActionResult Check([FromRoute] string name)
+        {
+            bool success = CustomerRepo.IsExist(name);
+            return success ? Ok(new{message= "is exist!"}) : BadRequest();
+        }
     }
 }
---
+++ Tuto_06.WebApi/Repos/CustomerRepo.cs
@@ -0,0 +1,17 @@
+﻿using Database2.Models;
+using Microsoft.IdentityModel.Abstractions;
+
+namespace Tuto_06.WebApi.Repos
+{
+    public class CustomerRepo
+    {
+        private static readonly AppDaContext _db = new AppDaContext();
+
+        public static bool IsExist(string customerName)
+        {
+            var res = CustomerRepo._db.Customers.Where(c => c.CustomerId.ToString() == customerName.ToString()).FirstOrDefault();
+
+            return (res is not null ) ? true : false;
+        }
+    }
+}
